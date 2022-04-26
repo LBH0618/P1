21da3cdb11c27b51c8d0c12ea393709d8eadb20d
LBH <102979694+LBH0618@users.noreply.github.com>
2022-04-26 21:29:11 +0800

    Modification Final Version

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # INFS7450 Social Media Analytics
 # Project 1 – Fast Computation of User Centrality
 # Banghong Liang
-# s4633687
+# 46336873
 # 2022/04/06
 from collections import deque
 from heapq import nlargest
 import networkx as nx
-import numpy as np
 import matplotlib.pyplot
 
 
@@ -18,63 +17,67 @@ def betweenness_centrality_calculation(graph):
     """
     if len(graph) == 0:
         return {}
-    # Create dictionary for graph with
-    # key - node
-    # value - betweenness centrality for the node
+    # Initialization
     dict_node_value = dict.fromkeys(graph, 0.0)
-    # Create a dummy graph to work with
-    dummy_graph = graph
     # Iterate through each vertex
-    for s in dummy_graph:
+    for single_source in dict_node_value:
         # Breadth first search for shortest path
-        S, P, sigma, _ = _breadth_first_search(graph, s)
-        # Accumulate the values
-        dict_node_value, delta = _accumulate(dict_node_value, S, P, sigma, s)
+        visited_vertices, predecessors, sigma, distance = _breadth_first_search(graph, single_source)
+        # Accumulate the values -> back-propagation of dependencies
+        dict_node_value = _accumulate(dict_node_value, visited_vertices, predecessors, sigma, single_source)
     # return dictionary for node's betweenness centrality
     return dict_node_value
 
 
-def _breadth_first_search(graph, s):
+def _breadth_first_search(graph, source):
+    """ Using breadth first search to find the shortest path for the single-source
+    :param graph: graph for facebook networks
+    :param source: single source
+    :return: visited_vertices, predecessors, sigma, distance
     """
-    :param graph: networks
-    :param s:
-    :return:
-    """
-    S = []  # list
-    P = {}  # dictionary
-
     # Initialization
+    predecessors = {}  # list of predecessors on shortest paths from source
     for vertex in graph:
-        P[vertex] = []
+        predecessors[vertex] = []
     sigma = dict.fromkeys(graph, 0.0)
-    D = {s: 0}  # dictionary
-    sigma[s] = 1.0  # visited
-    queue_bfs = deque([s])
+    sigma[source] = 1.0  # sigma[s] <- 1
+    queue_bfs = deque([source])  # enqueue s -> queue
+    visited_vertices = []
+    distance = {}
     while queue_bfs:
         vertex = queue_bfs.popleft()
-        S.append(vertex)
-        d_vertex = D[vertex]
-        sigma_vertex = sigma[vertex]
-        for w in graph[vertex]:
-            if w not in D:
-                queue_bfs.append(w)
-                D[w] = d_vertex + 1
-            if D[w] == d_vertex + 1:
-                sigma[w] += sigma_vertex
-                P[w].append(vertex)
-    return S, P, sigma, D
+        if vertex == source:
+            distance[vertex] = 0  # dist[s] <- 0
+        visited_vertices.append(vertex)
+        for vertex_w in graph[vertex]:
+            if vertex_w not in distance:  # if dist[w] = infinite then
+                distance[vertex_w] = distance[vertex] + 1  # dist[w] <- dist[v] + 1
+                queue_bfs.append(vertex_w)
+            if distance[vertex_w] == distance[vertex] + 1:
+                sigma[vertex_w] += sigma[vertex]  # sigma[w] <- sigma[w] + sigma[vertex]
+                predecessors[vertex_w].append(vertex)  # append vertex -> predecessors[w]
+    return visited_vertices, predecessors, sigma, distance
 
 
-def _accumulate(dict_node_value, S, P, sigma, s):
-    delta = dict.fromkeys(S, 0)
-    while S:
-        w = S.pop()
-        betweenness_coefficient = (1 + delta[w]) / sigma[w]
-        for vertex in P[w]:
-            delta[vertex] += sigma[vertex] * betweenness_coefficient
-        if w != s:
-            dict_node_value[w] += delta[w]
-    return dict_node_value, delta
+def _accumulate(dict_node_value, visited_vertices, predecessors, sigma, source):
+    """ Back-propagation of dependencies.
+    :param dict_node_value: dictionary for the graph
+    :param visited_vertices: list of visited vertices
+    :param predecessors: list of predecessors
+    :param sigma: number of shortest paths
+    :param source: source
+    :return: dict_node_value after accumulation
+    """
+    # dependence of source
+    dependency = dict.fromkeys(visited_vertices, 0)  # dependency[]
+    while visited_vertices:
+        vertex_w = visited_vertices.pop()  # pop w <- visited_vertices
+        for vertex in predecessors[vertex_w]:
+            # dependency[v] + sigma[v]/sigma[w] * (1 + dependency[w])
+            dependency[vertex] += sigma[vertex] / sigma[vertex_w] * (1 + dependency[vertex_w])
+        if vertex_w != source:
+            dict_node_value[vertex_w] += dependency[vertex_w] # Cb[w] + dependency[w]
+    return dict_node_value
 
 
 def pagerank_centrality_calculation(graph, alpha, beta):
@@ -93,23 +96,21 @@ def pagerank_centrality_calculation(graph, alpha, beta):
     # convert directed graph to right-stochastic graph
     # Normalize the edges for each node so that the sum of out edges of a node is equal to 1 (weight = 1).
     stochastic_graph = nx.stochastic_graph(dummy_graph)
-    num_nodes = dummy_graph.number_of_nodes()
+    num_nodes = dummy_graph.number_of_nodes() # number of nodes for the given graph
     # Normalized
     normalized_graph = dict.fromkeys(dummy_graph, 1.0 / num_nodes)
-
     # Power Iteration for Pagerank
-    for i in range(1000):
+    for i in range(100):
         normalized_graph_last = normalized_graph
         normalized_graph = dict.fromkeys(normalized_graph_last.keys(), 0)  # initialization
         for node in normalized_graph:
             for to_node in stochastic_graph[node]:
                 normalized_graph[to_node] += alpha * normalized_graph_last[node] \
                                              * stochastic_graph[node][to_node]['weight']
-            normalized_graph[node] += beta / num_nodes  # beta = 1 - alpha
+            normalized_graph[node] += beta / num_nodes  # beta = 0.15
         threshold = 0
         for node in normalized_graph:
             threshold += abs(normalized_graph[node] - normalized_graph_last[node])
-        # threshold = sum(abs(normalized_graph[node] - normalized_graph_last[node]) for node in normalized_graph)
         if num_nodes * 0.0000001 > threshold:
             return normalized_graph
 
@@ -144,18 +145,18 @@ if __name__ == '__main__':
 
     # Betweenness Centrality Calculation #
     # nx_q1_result = nx.betweenness_centrality(facebook_graph)
-    # q1_result = betweenness_centrality_calculation(facebook_graph)
+    q1_result = betweenness_centrality_calculation(facebook_graph)
 
     # Pagerank Centrality Calculation #
     # alpha = 0.85, beta = 0.15
-    nx_q2_result = nx.pagerank(facebook_graph)
+    # nx_q2_result = nx.pagerank(facebook_graph)
     q2_result = pagerank_centrality_calculation(facebook_graph, 0.85, 0.15)
 
     # Result Stream Out #
-    print(facebook_graph)
+    # print(facebook_graph)
     # Betweenness
-    # print(print_top_key(nx_q1_result))
-    # print(print_top_key(q1_result))
+    # print_top_key(nx_q1_result, 10)
+    print_top_key(q1_result, 10)
     # Pagerank
-    print_top_key(nx_q2_result, 10)
+    # print_top_key(nx_q2_result, 10)
     print_top_key(q2_result, 10)
